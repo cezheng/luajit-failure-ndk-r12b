0f81c1ea2839446804b40b20b00cf71460ee9cd5
Ce Zheng <cezheng.cs@gmail.com>
2016-08-04 19:55:30 +0900

    add comment for how to build with NDK r10e

--- jni/Application.mk
+++ jni/Application.mk
@@ -5,3 +5,6 @@ APP_CXXFLAGS := -frtti -fexceptions -std=c++11
 APP_LDFLAGS := -latomic
 NDK_TOOLCHAIN_VERSION := clang
 APP_PLATFORM := android-14
+# Just like r12b, NDK r10e cannot build this project either
+# but if you uncomment the following line, then this project can be built with NDK r10e
+#LIBCXX_USE_GABIXX := true
